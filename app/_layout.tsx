b20df5474c9afc5c1e45ad73a4e440b76dcc6024
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-07-09 21:41:18 +0100

    Add loading spinner to initial log in routing script, and route back to _layout.tsx on sign out. Also, refactor some code for cleaner implementation following review

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,51 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { Stack, useRouter } from "expo-router";
+import { Stack } from "expo-router";
+import { useState, useEffect } from "react";
 import { supabase } from "../supabase/auth-helper";
 import { Session } from "@supabase/supabase-js";
+import { View, ActivityIndicator } from "react-native";
 
 export default function RootLayout() {
   const [session, setSession] = useState<Session | null>(null);
   const [loading, setLoading] = useState(true);
-  const router = useRouter();
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      console.log("session fetched:", session);
+      console.log(session);
       setSession(session);
       setLoading(false);
-
-      if (session) {
-        router.replace("/(protected)");
-      } else {
-        router.replace("/sign-in");
-      }
     });
-
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session);
       setLoading(false);
-
-      if (session) {
-        router.replace("/(protected)");
-      } else {
-        router.replace("/sign-in");
-      }
     });
-
     return () => subscription.unsubscribe();
   }, []);
 
-  // if (loading) {
-  //   // TODO - Add loading spinner once Kitten UI Integrated
-  //   return <LoadingScreen />; // Placeholder for loading
-  // }
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   return (
     <Stack>
-      <Stack.Screen name="(app)" />
-      <Stack.Screen name="sign-in" />
+      <Stack.Protected guard={!!session}>
+        <Stack.Screen name="(protected)" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!session}>
+        <Stack.Screen name="sign-in" />
+      </Stack.Protected>
     </Stack>
   );
 }
--- supabase/supabase-signout.ts
+++ supabase/supabase-signout.ts
@@ -1,12 +1,16 @@
 import { supabase } from "./auth-helper";
+import { useRouter } from "expo-router";
 
 export async function handleSignOut() {
+  const router = useRouter();
   try {
     const { error } = await supabase.auth.signOut();
     if (error) {
       console.error("Error signing out:", error.message);
       // TODO - Handle error here - maybe back to login page?
+      return;
     }
+    router.replace("/sign-in");
   } catch (error) {
     console.error("Unexpected error during sign out:", error);
   }
