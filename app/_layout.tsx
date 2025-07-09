819d0e5e927b3ab329b5d3538492a96275541198
Vnb123 <vanabg@yahoo.com>
2025-07-09 12:45:01 +0000

    Added custom icons to the tab navigation bar and updated the titles of each tab screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,68 @@
-import { Tabs } from 'expo-router';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import React from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { Tabs } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider } from "@ui-kitten/components";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet } from "react-native";
 
 export default function RootLayout() {
   return (
     <ApplicationProvider {...eva} theme={eva.dark}>
       <SafeAreaProvider>
-        <React.Fragment>
-          <Tabs>
-            <Tabs.Screen name="home" />
-            <Tabs.Screen name="dashboard" />
-            <Tabs.Screen name="library" />
-            <Tabs.Screen name="new-session" />
-          </Tabs>
-        </React.Fragment>
+        <SafeAreaView style={page.container}>
+          <React.Fragment>
+            <Tabs>
+              <Tabs.Screen
+                name="index"
+                options={{
+                  title: "Home",
+                  tabBarIcon: ({ color, size }) => (
+                    <AntDesign name="home" size={24} color="black" />
+                  ),
+                }}
+              />
+              <Tabs.Screen
+                name="dashboard"
+                options={{
+                  title: "Dashboard",
+                  tabBarIcon: ({ color, size }) => (
+                    <AntDesign name="dashboard" size={24} color="black" />
+                  ),
+                }}
+              />
+              <Tabs.Screen
+                name="library"
+                options={{
+                  title: "Library",
+                  tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="library-outline" size={24} color="black" />
+                  ),
+                }}
+              />
+              <Tabs.Screen
+                name="new-session"
+                options={{
+                  title: "New Session",
+                  tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="create-outline" size={24} color="black" />
+                  ),
+                }}
+              />
+            </Tabs>
+          </React.Fragment>
+        </SafeAreaView>
       </SafeAreaProvider>
     </ApplicationProvider>
   );
 }
+const page = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#fff",
+    height: "100%",
+    width: "100%",
+  },
+});
