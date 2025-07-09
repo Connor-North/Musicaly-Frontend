15a3d8612d61346b09f0bffebf5df24d9cf8c2c9
Nazifah1 <135221826+Nazifah1@users.noreply.github.com>
2025-07-09 09:19:55 +0100

    adding dashboard page

---
+++ app/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Dashboard() {
+  return (
+    <SafeAreaView className="justify-center flex-1 p-4">
+      <Text> Dashboard</Text>
+    </SafeAreaView>
+  );
+}
