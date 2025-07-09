a5c2106e013d95e22d527f9b4584d3b7e72f5586
Vnb123 <vanabg@yahoo.com>
2025-07-09 15:57:44 +0000

    Added a tabBarLabel for more organized design

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout() {
               name="index"
               options={{
                 title: "Home",
+                tabBarLabel: "Home",
                 tabBarIcon: ({ color, size }) => (
                   <AntDesign name="home" size={24} color="black" />
                 ),
@@ -26,6 +27,7 @@ export default function RootLayout() {
               name="dashboard"
               options={{
                 title: "Dashboard",
+                tabBarLabel: "Dashboard",
                 tabBarIcon: ({ color, size }) => (
                   <AntDesign name="dashboard" size={24} color="black" />
                 ),
@@ -35,6 +37,7 @@ export default function RootLayout() {
               name="library"
               options={{
                 title: "Library",
+                tabBarLabel: "Library",
                 tabBarIcon: ({ color, size }) => (
                   <Ionicons name="library-outline" size={24} color="black" />
                 ),
@@ -44,6 +47,7 @@ export default function RootLayout() {
               name="new-session"
               options={{
                 title: "New Session",
+                tabBarLabel: "New Session",
                 tabBarIcon: ({ color, size }) => (
                   <Ionicons name="create-outline" size={24} color="black" />
                 ),
