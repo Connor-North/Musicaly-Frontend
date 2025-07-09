ae67301847c201c75a9e153ee5723585647a24dc
Nazifah1 <135221826+Nazifah1@users.noreply.github.com>
2025-07-09 09:20:46 +0100

    adding library page

---
+++ app/library.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Library() {
+  return (
+    <SafeAreaView className="justify-center flex-1 p-4">
+      <Text> Library</Text>
+    </SafeAreaView>
+  );
+}
