84db8697b06b53c6ece1c5b7f6e66a9d9bcb6d0b
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-07-11 10:16:04 +0100

    Add first rendering and placeholder of library list

--- app/(protected)/library.tsx
+++ app/(protected)/library.tsx
@@ -1,13 +1,16 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
+import UnitList from "@/components/units/UnitList";
 
 export default function Library() {
   return (
     <SafeAreaView
       className="justify-center flex-1 p-4"
       style={styles.container}
-    ></SafeAreaView>
+    >
+      <UnitList />
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
