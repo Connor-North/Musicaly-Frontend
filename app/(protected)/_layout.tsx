60fd6b8a2043ae1bdffbe927e2020e1e07a66879
Vnb123 <vanabg@yahoo.com>
2025-07-11 13:21:59 +0000

    removed eva icons

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout() {
   );
 
   const renderMenuAction = (): React.ReactElement => (
-    <TopNavigationAction icon={MenuIcon} onPress={toggleMenu} />
+    <TopNavigationAction onPress={toggleMenu} />
   );
   const renderOverflowMenuAction = (): React.ReactElement => (
     <OverflowMenu
@@ -40,7 +40,7 @@ export default function RootLayout() {
       visible={menuVisible}
       onBackdropPress={toggleMenu}
     >
-      <MenuItem accessoryLeft={LogOutIcon} title="Logout" />
+      <MenuItem title="Logout" />
     </OverflowMenu>
   );
   return (
