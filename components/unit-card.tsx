fb7d287f8abbd4b109a3d07ace6fffe9f03b99e9
Connor North <153465136+CNDR96@users.noreply.github.com>
2025-07-14 15:52:48 +0100

    Updated unit card to take props

--- components/unit-card.tsx
+++ components/unit-card.tsx
@@ -1,12 +1,17 @@
-import { StyleSheet, View, Text } from 'react-native';
-import React from 'react';
-import { Card } from '@ui-kitten/components';
+import { StyleSheet, View, Text } from "react-native";
+import React from "react";
+import { Card } from "@ui-kitten/components";
 
-export default function UnitCard() {
+type UnitCardProps = {
+  artist: string;
+  title: string;
+};
+
+export default function UnitCard({ artist, title }: UnitCardProps) {
   return (
     <Card style={styles.card}>
-      <Text>Oasis</Text>
-      <Text>Wonderwall</Text>
+      <Text>{artist}</Text>
+      <Text>{title}</Text>
     </Card>
   );
 }
@@ -14,8 +19,8 @@ export default function UnitCard() {
 const styles = StyleSheet.create({
   card: {
     borderWidth: 5,
-    paddingLeft: '10%',
-    paddingRight: '10%',
-    margin: '2%',
+    paddingLeft: "10%",
+    paddingRight: "10%",
+    margin: "2%",
   },
 });
