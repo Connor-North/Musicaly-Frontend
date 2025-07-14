cba5b5d023688151dbb9cff5ae7b5edb5b2282fa
Nazifah1 <135221826+Nazifah1@users.noreply.github.com>
2025-07-14 17:07:11 +0100

    update archive info

--- app/(protected)/library.tsx
+++ app/(protected)/library.tsx
@@ -1,8 +1,9 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, Modal, View, Text } from 'react-native';
+import { StyleSheet, Modal, View, Text, TextInput } from 'react-native';
 import UnitList from '@/components/units/UnitList';
 import React from 'react';
 import UnitCard from '@/components/unit-card';
+import { Input } from '@ui-kitten/components';
 
 export default function Library() {
   const [modalVisible, setModalVisible] = React.useState(false);
@@ -10,6 +11,7 @@ export default function Library() {
   const toggleModal = (): void => {
     setModalVisible(!modalVisible);
   };
+  const [note, setNote] = React.useState<string>('');
 
   return (
     <SafeAreaView
@@ -30,12 +32,21 @@ export default function Library() {
           style={styles.mainView}
           className="flex-1 items-center justify-center"
         >
-          <UnitCard />
+          <UnitCard artist="Beethoven" title="Moonlight Sonata" />
           <Text>Total Time played: 3 Hours </Text>
-          <Text>First played: (dskjcvds) </Text>
-          <Text>Last played: (dskjcvds) </Text>
-          <Text> Moonlight Sonata</Text>
-          <Text>Comments:</Text>
+          <Text>First played: 25 January 2025 </Text>
+          <Text>Last played: 30 January 2025 </Text>
+          <View>
+            <Text>Comments:</Text>
+            <Input
+              multiline={true}
+              textStyle={styles.inputTextStyle}
+              placeholder="Add Notes"
+              value={note}
+              onChangeText={(value) => setNote(value)}
+              style={{ width: '80%' }}
+            />
+          </View>
         </View>
       </Modal>
       )
@@ -52,5 +63,12 @@ const styles = StyleSheet.create({
   mainView: {
     justifyContent: 'center',
     flex: 1,
+  },
+  input: {
+    height: 10,
+    borderWidth: 3,
+  },
+  inputTextStyle: {
+    minHeight: 100,
   },
 });
