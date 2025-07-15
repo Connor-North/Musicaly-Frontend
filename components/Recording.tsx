ded00232c1251b3e720898bbb3e5d1d18c2982b1
Vnb123 <vanabg@yahoo.com>
2025-07-15 15:09:39 +0000

    Working on adding a playback to the recording

--- components/Recording.tsx
+++ components/Recording.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { Audio } from "expo-av";
 import { Button } from "@ui-kitten/components";
+import { StatusBar } from "expo-status-bar";
 
+type SingleRecording = {
+  sound: Audio.Sound;
+  duration: String;
+  file: String | null;
+};
 export default function Recording() {
-  const [recording, setRecording] = useState();
-  const [recordings, setRecordings] = useState([]);
+  const [recording, setRecording] = useState<Audio.Recording | null>(null);
+  const [recordings, setRecordings] = useState<SingleRecording[]>([]);
   const [permissionResponse, requestPermission] = Audio.usePermissions();
   const [message, setMessage] = useState("");
 
@@ -33,20 +39,54 @@ export default function Recording() {
 
   async function stopRecording() {
     console.log("Stopping recording..");
-    setRecording(undefined);
+    if (!recording) return;
+    setRecording(null);
     await recording.stopAndUnloadAsync();
     await Audio.setAudioModeAsync({
       allowsRecordingIOS: false,
     });
-    const uri = recording.getURI();
-    console.log("Recording stopped and stored at", uri);
+    let updatedRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    if (!status.isLoaded || typeof status.durationMillis !== "number") {
+      setRecording(null);
+      return;
+    }
+    updatedRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI(),
+    });
+    setRecordings(updatedRecordings);
   }
-
+  function getDurationFormatted(millis: number) {
+    if (!millis || isNaN(millis)) return "0: 00";
+    const minutes = millis / 1000 / 60;
+    const minutesDisplay = Math.floor(minutes);
+    const seconds = Math.round((minutes - minutesDisplay) * 60);
+    const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+    return `${minutesDisplay} : ${secondsDisplay}`;
+  }
+  function getRecordingLines() {
+    return recordings.map((recordingLine, index) => {
+      return (
+        <View key={index}>
+          <Text>
+            Recording {index + 1} - {recordingLine.duration}
+          </Text>
+          <Button onPress={() => recordingLine.sound.replayAsync()}>
+            Play
+          </Button>
+        </View>
+      );
+    });
+  }
   return (
     <View style={styles.container}>
       <Button onPress={recording ? stopRecording : startRecording}>
         {recording ? "⬛" : "🔴"}
       </Button>
+      {getRecordingLines()}
+      <StatusBar style="auto" />
     </View>
   );
 }
