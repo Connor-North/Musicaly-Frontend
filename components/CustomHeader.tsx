a4ef3b58342ae589ce91fa194605568c1ab34bbc
Nazifah1 <135221826+Nazifah1@users.noreply.github.com>
2025-07-16 15:53:15 +0100

    adding logo and title to navigation page

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,7 +1,9 @@
 import { handleSignOut } from "./../supabase/supabase-signout";
 import React from "react";
-
+import { StyleSheet } from "react-native";
 import { TopNavigation, Button } from "@ui-kitten/components";
+import MusicalyLogo from "./MusicalyLogo";
+
 export default function CustomHeader() {
   const [disableSignOutButton, setDisableSignOutButton] = React.useState(false);
 
@@ -12,8 +14,25 @@ export default function CustomHeader() {
   }
 
   const renderSignOut = (): React.ReactElement => (
-    <Button onPress={signOut}>Logout</Button>
+    <>
+      <Button onPress={signOut} size="tiny">
+        Logout
+      </Button>
+    </>
   );
 
-  return <TopNavigation accessoryRight={renderSignOut} />;
+  return (
+    <TopNavigation
+      accessoryRight={renderSignOut}
+      alignment="center"
+      title="Musicaly"
+      accessoryLeft={() => <MusicalyLogo />}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    height: 20,
+  },
+});
